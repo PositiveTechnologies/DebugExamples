2678705e84039ef8e5474ebc7036ba040de077b8
Mikhail Polgun <mikhailpolgun@gmail.com>
2017-03-29 22:42:00 +0300

    Cpu utilization application draft

---
+++ CpuUtilization/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpuUtilization
+{
+	class Program
+	{
+		private static ConcurrentDictionary<Guid, Asset> _cache = new ConcurrentDictionary<Guid, Asset>();
+
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Example of a heavy cpu utilization application.");
+			Console.WriteLine("Press Ctrl+C for exit.\n");
+			Console.WriteLine($"ProcessId = {Process.GetCurrentProcess().Id}, {ProcessorArchticture()}");
+
+			var processorCount = Environment.ProcessorCount;
+			var tasks = new Task[processorCount];
+
+			for (int i = 0; i < processorCount; i++)
+			{
+				var task = Task.Factory.StartNew(DoSomeHeavyWork, TaskCreationOptions.LongRunning);
+				tasks[i] = task;
+			}
+			
+			Task.WaitAll(tasks);
+		}
+
+		private static void DoSomeHeavyWork()
+		{
+			while (true)
+			{
+				ResolveAsset(Guid.NewGuid());
+			}
+		}
+
+		private static string ProcessorArchticture()
+		{
+			return IntPtr.Size == 8 ? "x64" : "x86";
+		}
+
+		private static Asset ResolveAsset(Guid assetId)
+		{
+			try
+			{
+				return _cache[assetId];
+			}
+			catch (KeyNotFoundException)
+			{
+				Init(assetId);
+			}
+
+			return _cache[assetId];
+		}
+
+		private static void Init(Guid assetId)
+		{
+			_cache.TryAdd(assetId, new Asset());
+		}
+	
+	}
+
+	class Asset { }
+}
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ A bunch of C# projects to illustrate debugging with SOS and SosEx
 * **MonitorDeadlock** - simple deadlock example on lock (Monitor)
 * **RwlDeadlock** - some deadlock examples on ReaderWriterLock and |ReaderWriterLockSlim 
 * **MemoryLeak** - simple example of managed memory leak
+* **CpuUtilization** - example of a heavy cpu utilization application
